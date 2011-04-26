cd665b3fee53a36891f90b1dd85ab85f54713bf2
Stewart McKee <stewart@theizone.co.uk>
2011-04-26 17:54:06 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BrewingDiary::Application.config.secret_token = '2b969fb2151d0c77df4a1cd6db5aca7b7e0352683497ddf5061a1093e7a2f30cda63a73b189e2156adb6e1417db990aeeb7b3526df3a44ea8daa9783062976d8'
