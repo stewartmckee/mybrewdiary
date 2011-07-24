996287694be7bb4063fd51f3e4f035ace09b4366
Stewart McKee <stewart@theizone.co.uk>
2011-07-24 11:46:44 +0100

    adusting date and brewing dashboard

--- app/helpers/brewings_helper.rb
+++ app/helpers/brewings_helper.rb
@@ -1,7 +1,7 @@
 module BrewingsHelper
 
   def date_in_words(date)
-    if date < DateTime.now - 7.days
+    if date < DateTime.now + 7.days
       words = ""
       words += "in " if date > DateTime.now
       words += time_ago_in_words(date).strip
