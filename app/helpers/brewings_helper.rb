70cc6c5396210a973cc034c061274e4f7fd7ec69
Stewart McKee <stewart@theizone.co.uk>
2011-07-24 11:41:08 +0100

    changed older dates to show the actual date rather than date in words

--- app/helpers/brewings_helper.rb
+++ app/helpers/brewings_helper.rb
@@ -1,11 +1,15 @@
 module BrewingsHelper
 
   def date_in_words(date)
-    words = ""
-    words += "in " if date > DateTime.now
-    words += time_ago_in_words(date).strip
-    words += " ago" if date < DateTime.now
-    words
+    if date > DateTime.now + 7.days
+      words = ""
+      words += "in " if date > DateTime.now
+      words += time_ago_in_words(date).strip
+      words += " ago" if date < DateTime.now
+      words
+    else
+      "#{date.day.ordinalize} #{date.strftime("%B %Y")}"
+    end
   end
   
 end
