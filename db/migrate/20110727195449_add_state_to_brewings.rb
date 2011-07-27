dfaa54fa32c534e8a6ad2f768ef7d0555cc48875
Stewart McKee <stewart@theizone.co.uk>
2011-07-27 21:56:48 +0100

    updated migration to set state to correct value

--- db/migrate/20110727195449_add_state_to_brewings.rb
+++ db/migrate/20110727195449_add_state_to_brewings.rb
@@ -1,6 +1,16 @@
 class AddStateToBrewings < ActiveRecord::Migration
   def self.up
     add_column :brewings, :state, :string
+    
+    Brewing.all.each do |brewing|
+      if brewing.bottled_on > DateTime.now
+        brewing.update_attributes(:state => "PRIMARY")
+      elsif brewing.ready_on > DateTime.now
+        brewing.update_attributes(:state => "SECONDARY")
+      else
+        brewing.update_attributes(:state => "READY")
+      end
+    end
   end
 
   def self.down
